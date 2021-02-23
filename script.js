a41726e61397180fc86d5c009a533baaec4f317e
Jeffrey Wang <69502476+wangjeffreyd@users.noreply.github.com>
2021-02-23 15:01:23 -0700

    HW 20
    Homework 20 changes

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }    
 
-        
-    })
-});
